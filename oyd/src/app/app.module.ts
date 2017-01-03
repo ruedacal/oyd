218c225202143590a6badc10733f45c969cfa0e6
Victor R <rueda.v@gmail.com>
2017-01-03 15:17:26 +0100

    New component & bootstrap + query

--- oyd/src/app/app.module.ts
+++ oyd/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormularioinicialComponent } from './formularioinicial/formularioinicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioinicialComponent
   ],
   imports: [
     BrowserModule,
---
+++ oyd/src/app/formularioinicial/formularioinicial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formularioinicial',
+  templateUrl: './formularioinicial.component.html',
+  styleUrls: ['./formularioinicial.component.css']
+})
+export class FormularioinicialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
